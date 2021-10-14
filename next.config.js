afd0707fc82f702d1992f01332367cc031987aec
kstee <kstee_90@hotmail.com>
2021-10-14 12:14:06 +0800

    hide mapbox key

--- next.config.js
+++ next.config.js
@@ -2,8 +2,5 @@ module.exports = {
   images: {
     domains: ["links.papareact.com"],
   },
-  env: {
-    mapbox_key:
-      "pk.eyJ1IjoibHVjYXNraG9vIiwiYSI6ImNrdXA3bWQ4bTMwbDUycnQ0Y2ttc3U5ZzcifQ.WlLvpogBij4j1ty3ZlwDfg",
-  },
+  env: {},
 };
