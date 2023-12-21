629c711e9e257d746282b5551b87b98bb3b3bcf3
kstee <kstee_90@hotmail.com>
2023-12-21 15:10:19 +0800

    Changed the link of exploreData in index.js

--- pages/search.js
+++ pages/search.js
@@ -65,7 +65,7 @@ function Search({ searchResults }) {
 export default Search;
 
 export async function getServerSideProps() {
-  const searchResults = await fetch("https://www.jsonkeeper.com/b/5NPS").then(
+  const searchResults = await fetch("https://links.papareact.com/isz").then(
     (res) => res.json()
   );
 
