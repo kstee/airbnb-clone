94b6ac085b81c97e61b64fba31c27dd5edae0266
kstee <kstee_90@hotmail.com>
2023-12-21 14:57:43 +0800

    Changed the link of exploreData in index.js

--- pages/index.js
+++ pages/index.js
@@ -58,7 +58,7 @@ export default function Home({ exploreData, cardsData }) {
 }
 
 export async function getStaticProps() {
-  const exploreData = await fetch("https://links.papareact.com/pyp").then(
+  const exploreData = await fetch("https://www.jsonkeeper.com/b/4G1G").then(
     (res) => res.json()
   );
 
